bc9231ee1d6a522944ec72e7c0522b2b051bd0b9
Vamsi <s.vamsirao@gmail.com>
2017-09-06 16:58:43 +0530

    Set style and respond to search term

--- src/index.js
+++ src/index.js
@@ -13,19 +13,31 @@ const API_KEY= 'AIzaSyDZOckA7NBJ5M0SvxAZZSBz9JsUdDb5Jn4';
 	constructor(props){
 		super(props);
 
-		this.state= { videos: [] };
+		this.state= {
+			 videos: [],
+			 selectedVideo: null
+			 };
 
-		YTSearch({key: API_KEY, term:'surfboards'}, videos=>{
-			this.setState({ videos });
-		});
-
+		this.videoSearch('musk');
 	}
+
+	videoSearch(term){
+		YTSearch({key: API_KEY, term: term}, videos=>{
+			this.setState({ 
+				videos: videos,
+				selectedVideo: videos[0]
+			 });
+		});
+	}
+
 	render(){
 		return ( 
 			<div>
-				<SearchBar/>
-				<VideoDetail video={this.state.videos[0]} />
-				<VideoList videos={this.state.videos} />
+				<SearchBar onSearchTermChange={term=>this.videoSearch(term)}/>
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					onVideoSelect={(selectedVideo)=>this.setState({selectedVideo})}
+					videos={this.state.videos} />
 			</div> 
 		);
 	}
